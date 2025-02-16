ccd8b296670a873b069882427fbc69bcd0a98802
Lilith Orion Hafner <lilithhafner@gmail.com>
2025-02-16 07:07:56 -0600

    Initial code

--- src/ChairmarksExtras.jl
+++ src/ChairmarksExtras.jl
@@ -1,5 +1,64 @@
 module ChairmarksExtras
 
-# Write your package code here.
+using Reexport
+@reexport using Chairmarks
+
+export @belapsed, @ballocated, @ballocations, @btime, @btimed
+
+macro belapsed(args...)
+    :(@b($(args...)).time)
+end
+macro ballocated(args...)
+    :(Int(@b($(args...)).bytes))
+end
+macro ballocations(args...)
+    :(Int(@b($(args...)).allocs))
+end
+
+function save_result(expr)
+    result = gensym("result")
+    args = expr.args[1].args
+    length(args) == 2 && return expr
+    length(args) == 3 && insert!(args, 3, nothing)
+    while length(args) < 5; push!(args, nothing) end
+    i = lastindex(args)
+    if args[i] === nothing
+        args[i] = :(Base.Fix1(setindex!, $result))
+    elseif args[i] isa Expr && args[i].head == :->
+        args[i].args[2] = :($result[] = $(args[i].args[2]))
+    else
+        x = gensym()
+        args[i] = :($x -> $result[] = $(args[i])($x))
+    end
+    quote
+        $(esc(result)) = Ref{Any}()
+        Chairmarks.summarize($expr), $(esc(result))[]
+
+    end
+end
+
+macro btimed(args...)
+    quote
+        perf,value = $(save_result(Chairmarks.process_args(args)))
+        (
+            value=value,
+            time=perf.time,
+            bytes=perf.bytes,
+            gctime=perf.gc_fraction*perf.time,
+            allocations=perf.allocs,
+            compile_time=perf.compile_fraction*perf.time,
+            recompile_time=perf.time*perf.compile_fraction*perf.recompile_fraction,
+        )
+    end
+end
+macro btime(args...)
+    quote
+        perf,value = $(save_result(Chairmarks.process_args(args)))
+        print("  ")
+        show(stdout, MIME"text/plain"(), perf)
+        println()
+        value
+    end
+end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,10 +1,32 @@
 using ChairmarksExtras
 using Test
-using Aqua
 
 @testset "ChairmarksExtras.jl" begin
-    @testset "Code quality (Aqua.jl)" begin
-        Aqua.test_all(ChairmarksExtras)
+    @test (@belapsed 1+1) isa Float64
+    @test 0 < (@belapsed 1+1) < 100
+    @test 0 === @ballocated 1+1
+    @test 0 === @ballocations 1+1
+    @test 0 < @ballocated rand(10)
+    @test 0 < @ballocations rand(10)
+
+    p = Pipe()
+    redirect_stdout(p) do
+        @test 2 == @btime 1+1
     end
-    # Write your tests here.
+    close(p.in)
+    s = read(p.out, String)
+    m = match(r"^  (\d+\.\d\d\d (n|μ|m|))s$", s).captures[1]
+    @test s == "  "*m*"s\n"
+
+    @test (@btimed 5 sqrt _^2 @assert _ ≈ 5).value === nothing
+    @test (@btimed 5 sqrt _^2 inv).value ≈ 1/5
+    @test (@btimed 5 _^2 inv).value ≈ 1/25
+    @test_throws MethodError @btimed
+
+    @testset "Aqua" begin
+        import Aqua
+        # persistent_tasks=false because that test is slow and we don't use persistent tasks
+        Aqua.test_all(ChairmarksExtras, deps_compat=false, persistent_tasks=false)
+        Aqua.test_deps_compat(ChairmarksExtras, check_extras=false)
+    end
 end
